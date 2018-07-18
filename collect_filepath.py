450835ddf05e9ae58f435454d1d13110182c2de2
sogohu <40509298+sogohu@users.noreply.github.com>
2018-07-19 00:13:06 +0800

    Add files via upload

--- collect_filepath.py
+++ collect_filepath.py
@@ -12,12 +12,14 @@ for d1 in train_dirs:
     for d2 in os.listdir("./{0}".format(d1)):
         if d2 == "正常":
             for f in os.listdir("./{0}/{1}".format(d1, d2)):
-                true_count += 1
-                f_true.write("./{0}/{1}/{2}\n".format(d1, d2, f))
+                if f.lower().endswith(".jpg"):
+                    true_count += 1
+                    f_true.write("./{0}/{1}/{2}\n".format(d1, d2, f))
         else:
             for f in os.listdir("./{0}/{1}".format(d1, d2)):
-                false_count += 1
-                f_false.write("./{0}/{1}/{2}\n".format(d1, d2, f))
+                if f.lower().endswith(".jpg"):
+                    false_count += 1
+                    f_false.write("./{0}/{1}/{2}\n".format(d1, d2, f))
 
 print("true_count={0}, false_count={1}".format(true_count, false_count))
 f_true.close()
